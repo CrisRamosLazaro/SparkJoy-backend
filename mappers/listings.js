99d353537acc1788d78123dec021f49b6d4a1286
CrisRamosLazaro <crislr@hotmail.com>
2024-11-06 19:23:16 +0100

    url typo fix

--- mappers/listings.js
+++ mappers/listings.js
@@ -4,7 +4,7 @@ const baseUrl = process.env.NODE_ENV === 'production' ? process.env.BASE_URL : p
 
 const mapper = listing => {
   const mapImage = image => ({
-    url: `${baseUrl}${image.fileName}_full.jpg`,
+    url: `${baseUrl}/${image.fileName}_full.jpg`,
     thumbnailUrl: `${baseUrl}${image.fileName}_thumb.jpg`
   })
 
